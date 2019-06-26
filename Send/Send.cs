6c32de9806880824548f10e3ec9d5b76ce342c9c
Pablo Vicente <pablo.vicente@grad.ufsc.br>
2019-06-26 17:15:52 -0300

    Add

--- Send/Send.cs
+++ Send/Send.cs
@@ -12,22 +12,15 @@ namespace Send
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "hello",
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+                channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
                 string message = "Helllo World";
                 var body = Encoding.UTF8.GetBytes(message);
 
-                channel.BasicPublish(exchange: "",
-                    routingKey: "hello",
-                    basicProperties: null,
-                    body: body);
+                channel.BasicPublish(exchange: "logs",routingKey: "",basicProperties: null,body: body);
                 Console.WriteLine(" [X] Sent {0}",message);
             }
             Console.WriteLine(" Press [ENTER] to exit.");
-            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
